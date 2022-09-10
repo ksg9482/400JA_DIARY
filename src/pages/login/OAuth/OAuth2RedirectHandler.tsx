c357b162cb0ca0737529e4cdd02a723660acb227
ksg9482 <ksg930523@gmail.com>
2022-09-10 09:17:25 +0900

    update

--- src/pages/login/OAuth/OAuth2RedirectHandler.tsx
+++ src/pages/login/OAuth/OAuth2RedirectHandler.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-const OAuth2RedirectHandler = (props:any) => {
-    //소셜로그인 상태관리는 어떻게? 상위에서 보내줘야 하는데?
-    const code = new URL(window.location.href).searchParams.get("code");
-    console.log(code)
-    return(
-        <div></div>
-    )
-}
+import axios from "axios";
+const OAuth2RedirectHandler = (props: any) => {
+  //소셜로그인 상태관리는 어떻게? 상위에서 보내줘야 하는데?
+  const code = new URL(window.location.href).searchParams.get("code");
+  const sendCode = axios.post(`http://localhost:8080/api/auth/kakao`, code, {
+    withCredentials: true,
+  });
+  console.log(code);
+  return <div></div>;
+};
 
-export default OAuth2RedirectHandler
+export default OAuth2RedirectHandler;
