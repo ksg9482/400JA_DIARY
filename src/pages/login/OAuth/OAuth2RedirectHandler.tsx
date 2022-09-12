4b81bf463a36b2b36efdd2623f21d96550ea6400
ksg9482 <ksg930523@gmail.com>
2022-09-12 21:42:00 +0900

    oauth 프론트엔드

--- src/pages/login/OAuth/OAuth2RedirectHandler.tsx
+++ src/pages/login/OAuth/OAuth2RedirectHandler.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import axios from "axios";
 const OAuth2RedirectHandler = (props: any) => {
   //소셜로그인 상태관리는 어떻게? 상위에서 보내줘야 하는데?
+  //리다이렉트창 노출될 수 있으니 로딩창 만들어서 넣기
+  //카카오인지 구글인지 알수 있어야 한다.
   const code = new URL(window.location.href).searchParams.get("code");
-  const sendCode = axios.post(`http://localhost:8080/api/auth/kakao`, code, {
+  const sendCode = axios.get(`http://localhost:8080/api/auth/kakao?code=${code}`, {
     withCredentials: true,
   });
   console.log(code);
