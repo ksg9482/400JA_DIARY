05e65d4a02ee1e34a917824f507439dc72475b78
ksg9482 <ksg930523@gmail.com>
2022-09-14 13:39:00 +0900

    save

--- src/pages/login/OAuth/OAuth2RedirectHandler.tsx
+++ src/pages/login/OAuth/OAuth2RedirectHandler.tsx
@@ -5,11 +5,13 @@ const OAuth2RedirectHandler = (props: any) => {
   //리다이렉트창 노출될 수 있으니 로딩창 만들어서 넣기
   //카카오인지 구글인지 알수 있어야 한다.
   const code = new URL(window.location.href).searchParams.get("code");
-  const oauthPath = new URL(window.location.href).pathname.split('/')
-  console.log(code)
-  async function handle() {
+  const oauthPath = new URL(window.location.href).pathname.split('/');
+  //자기호출도 안됨. 여기를 로그인쪽에서 하거나 백엔드로 옮기기
+  (async function handle() {
     if (oauthPath.includes('kakao')) {
+      //함수 안에 든걸 빼면? 유즈이펙트 때문에 2번 호출되는 거 같다.
       try {
+        console.log('프론트 호출')
         const sendCode = await axios.get(`http://localhost:8080/api/auth/kakao?code=${code}`, {
           withCredentials: true,
         });
@@ -27,10 +29,10 @@ const OAuth2RedirectHandler = (props: any) => {
         withCredentials: true,
       });
     };
-  }
-  useEffect(() => {
-    handle()
-  }, [])
+  })()
+  // useEffect(() => {
+  //   handle()
+  // }, [])
 
   return <div></div>;
 };
