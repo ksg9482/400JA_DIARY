27495fb00b9dbc8633e1f81fe4861d6422750812
ksg9482 <ksg930523@gmail.com>
2022-10-19 19:38:19 +0900

    save

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,6 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  // product 환경이 아니면 코드가 의도한대로 동작하지 않음. 
   //<React.StrictMode>
   <HelmetProvider>
     <App />
